dd4b337f6470d4df980bb917754c9e7b2e647d6c
Wei_Z <30371929+meadaatsuko@users.noreply.github.com>
2019-11-20 13:30:10 +0800

    Update getCurrentPosition.js

--- getCurrentPosition.js
+++ getCurrentPosition.js
@@ -1,10 +1,9 @@
 function getCurrentPositionEvent(argument) {
 	// body...
-	var getCurrentPosition = navigator.geolocation.getCurrentPosition;
 	function successPosition(Position) {
 		// body...
 		console.log(Position.coords.latitude);
 	}
-	getCurrentPosition(successPosition);
+	navigator.geolocation.getCurrentPosition(successPosition);
 }
 EventUtil.addEvent(window,"load",getCurrentPositionEvent);
