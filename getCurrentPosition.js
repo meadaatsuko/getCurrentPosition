68f30a77633b29d093ca8b055da9ae83e78e1166
Wei_Z <30371929+meadaatsuko@users.noreply.github.com>
2019-11-20 15:00:23 +0800

    Update getCurrentPosition.js

--- getCurrentPosition.js
+++ getCurrentPosition.js
@@ -1,15 +1,16 @@
 function getCurrentPositionEvent(argument) {
 	// body...
+	var posInfo = document.querySelector("p#position-info");
 	function successPosition(Position) {
 		// body...
 		var positionInfo = "";
-		positionInfo = "纬度: " + Position.coords.latitude + ", 经度: " + Position.coords.longitude + ", 海拔： " + Position.coords.altitude + ".";
-		console.log(positionInfo);
+		positionInfo = "纬度: " + Position.coords.latitude + ", 经度: " + Position.coords.longitude +  ".";
+		posInfo.innerHTML = positionInfo;
 	}
 	function errorPosition(error) {
 		// body...
-		console.log(error);
+		posInfo.innerHTML = error.message;
 	}
-	navigator.geolocation.getCurrentPosition(successPosition,errorPosition);
+	navigator.geolocation.watchPosition(successPosition,errorPosition);
 }
 EventUtil.addEvent(window,"load",getCurrentPositionEvent);
